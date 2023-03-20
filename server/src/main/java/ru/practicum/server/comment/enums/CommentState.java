6973f783bb32a7260d617fb91f6d0f976f0960f4
Evgeniy Budaev <budaev.e@gmail.com>
2023-03-20 07:09:18 +0300

    only stats

--- server/src/main/java/ru/practicum/server/comment/enums/CommentState.java
+++
@@ -1,5 +0,0 @@
-package ru.practicum.server.comment.enums;
-
-public enum CommentState {
-    NOT_EDIT, EDITED
-}
--- server/src/main/java/ru/practicum/server/report/service/ReportService.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.server.report.service;
-
-import ru.practicum.server.report.dto.ReportDto;
-
-public interface ReportService {
-    ReportDto getReportByUserId(Long userId);
-}
