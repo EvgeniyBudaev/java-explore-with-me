3783340db50d8aa746c018bf02e4321100dac5f2
Evgeniy Budaev <budaev.e@gmail.com>
2023-03-22 11:57:05 +0300

    test16

--- server/src/main/java/ru/practicum/server/ServerApplication.java
+++ server/src/main/java/ru/practicum/server/ServerApplication.java
@@ -2,8 +2,9 @@ package ru.practicum.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.practicum.client.event.statclient.StatisticClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {StatisticClient.class})
 public class ServerApplication {
 
     public static void main(String[] args) {
