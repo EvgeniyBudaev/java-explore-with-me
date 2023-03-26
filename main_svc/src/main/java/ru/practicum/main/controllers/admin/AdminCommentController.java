3702d4c47bb486b9d30641582c39acc44a0cddb2
Evgeniy Budaev <budaev.e@gmail.com>
2023-03-26 14:42:07 +0300

    fix by checkstyle

--- main_svc/src/main/java/ru/practicum/main/controllers/admin/AdminCommentController.java
+++ main_svc/src/main/java/ru/practicum/main/controllers/admin/AdminCommentController.java
@@ -1,7 +1,7 @@
 package ru.practicum.main.controllers.admin;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
