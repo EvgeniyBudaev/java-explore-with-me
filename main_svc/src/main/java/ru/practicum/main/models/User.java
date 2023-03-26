0cfce9c43cf471e90ace8351a094cbd384619836
Evgeniy Budaev <budaev.e@gmail.com>
2023-03-26 14:39:09 +0300

    fix by checkstyle

--- main_svc/src/main/java/ru/practicum/main/models/User.java
+++ main_svc/src/main/java/ru/practicum/main/models/User.java
@@ -6,11 +6,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "users", schema = "public")
