3b0c7b4c3ef84d99aab75d38a9dcc07fdec46ce9
Evgeniy Budaev <budaev.e@gmail.com>
2023-03-26 14:35:08 +0300

    Избавился от wildcard

--- stat_svc/stat_server/src/main/java/ru/practicum/server/models/EndpointHit.java
+++ stat_svc/stat_server/src/main/java/ru/practicum/server/models/EndpointHit.java
@@ -5,7 +5,11 @@ import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 import ru.practicum.server.constants.Pattern;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
